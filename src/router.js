45c8586e250b79eb5547b36918338bbd655b63ec
wildenali <wildenwildenaliali@gmail.com>
2020-09-14 22:40:34 +0700

    Membuat controller

--- src/router.js
+++ src/router.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("./controllers/controller");
 
-router.get("/", (req, res) => {
-  res.render("index", {
-    title: "Express Weather Finder",
-  });
-});
-
-router.get("/about", (req, res) => {
-  res.render("about");
-});
+router.get("/", controller.renderHomePage);
+router.get("/about", controller.renderAboutPage);
 
 module.exports = router;
