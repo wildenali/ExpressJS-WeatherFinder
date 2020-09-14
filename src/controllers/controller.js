a31ed637083f3955cc13bcea55e08c5c1537f6d1
wildenali <wildenwildenaliali@gmail.com>
2020-09-14 23:28:03 +0700

    Cara dapatkan parameter dari input tag yg di submit

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,6 +2,11 @@ exports.renderHomePage = (req, res) => {
   res.render("index");
 };
 
+exports.getWeather = (req, res) => {
+  console.log(req);
+  res.send(`You typed ${req.body.city}`);
+};
+
 exports.renderAboutPage = (req, res) => {
   res.render("about");
 };
