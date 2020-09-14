0bfb6ca117778cd290c4560ad3db85ff65f564c7
wildenali <wildenwildenaliali@gmail.com>
2020-09-14 17:32:37 +0700

    Buat route untuk /about dan ganti PORT

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Cek");
 });
 
-app.listen(3000);
+app.get("/about", (req, res) => {
+  res.send("Ini di About");
+});
+
+app.listen(PORT, () =>
+  console.log(`The server is now running on Port ${PORT}`)
+);
